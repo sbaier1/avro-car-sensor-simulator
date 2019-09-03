dc10dc25fffaa4a463f11c6d50aeaf150af5b5e1
sbaier <simon.baier@dc-square.de>
2019-09-03 18:04:50 +0200

    basic gradle setup for a project with an avro file, basic car data schema idea

---
+++ src/main/java/com/hivemq/Main.java
@@ -0,0 +1,11 @@
+package com.hivemq;
+
+import com.hivemq.avro.CarData;
+
+public class Main {
+    public static void main(String[] args) {
+        CarData.newBuilder()
+                // TODO generate data somehow and serialize it
+                .build();
+    }
+}
