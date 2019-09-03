10619846bfa63bae6c2917891d08f2753550c31c
sbaier <simon.baier@dc-square.de>
2019-09-03 18:50:52 +0200

    build sample and encode

--- src/main/java/com/hivemq/Main.java
+++ src/main/java/com/hivemq/Main.java
@@ -2,10 +2,20 @@ package com.hivemq;
 
 import com.hivemq.avro.CarData;
 
+import java.nio.ByteBuffer;
+
 public class Main {
-    public static void main(String[] args) {
-        CarData.newBuilder()
+    public static void main(String[] args) throws Exception {
+        final CarData build = CarData.newBuilder()
+                .setBatteryPercentage(100)
+                .setBatteryVoltage(200)
+                .setCurrentDraw(10)
+                .setSpeed(100)
+                .setIntakeAirTemp(30)
+                .setEngineCoolantTemp(90)
+                .setIntakeAirFlowSpeed(100)
                 // TODO generate data somehow and serialize it
                 .build();
+        final ByteBuffer encoded = CarData.getEncoder().encode(build);
     }
 }
